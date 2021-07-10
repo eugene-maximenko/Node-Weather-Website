9ccae5227fb42304d384a97e26adcb794ff17a17
tiger3120 <zhek4770@gmail.com>
2021-07-10 11:45:57 +0300

    Commented out the file

--- public/js/app.js
+++ public/js/app.js
@@ -3,14 +3,18 @@ const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 
+// Weather form submit event listener
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
 
+    // Render loading while waiting for data
     messageOne.textContent = 'Loading...';
     messageTwo.textContent = '';
 
+    // Get location from input
     const location = search.value;
     
+    // Fetch the forecast
     fetch(`/weather?address=${location}`)
 
         .then(response => {
@@ -18,8 +22,11 @@ weatherForm.addEventListener('submit', (event) => {
 
                 .then(data => {
 
+                    // Handle error
                     if (data.error) {
                         messageOne.textContent = data.error;
+
+                        // Display forecast
                     } else {
                         messageTwo.textContent = data.forecast;
                         messageOne.textContent = `${data.latitude}, ${data.longitude}`;
