95372408392b77d498cef9754efbf50fdf8a567b
tiger3120 <zhek4770@gmail.com>
2021-07-11 14:16:37 +0300

    Commented out the file

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,13 +1,23 @@
 const request = require('request');
 
+// Get the forecast by lat and lon
 const forecast = (latitude, longitude, callback) => {
+
+    // Url for request
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=4a29db7ec38cf0793a1bf581b2339e74`;
 
+    // Request
     request({ url, json: true }, (error, { body }) => {
+
+        // Handle error 
         if (error) {
             callback('Hm, here is a low error of request :(', undefined);
+
+            // Handle problems with coordinates
         } else if (body.message) {
             callback('Some problems with coordinates!', undefined);
+
+            // Return the forecast
         } else {
             callback(undefined, 'It is currently ' + Math.round(body.main.temp - 273.15) + ' degrees out. The max temperature today is ' + Math.round(body.main.temp_max - 273.15) + ' deegrees. There is a 0% chance of rain');
         }
